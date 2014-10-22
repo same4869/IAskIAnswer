6341ea3617b28c997da4cb6888fcbd8cd77ae05c
same4869 <same4869@126.com>
2014-10-22 23:46:21 +0800

    add baidu voice sdk

--- gen/com/xun/iaskianswer/R.java
+++ gen/com/xun/iaskianswer/R.java
@@ -18,21 +18,32 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+    }
+    public static final class id {
+        public static final int btn=0x7f080001;
+        public static final int tv=0x7f080000;
     }
     public static final class layout {
         public static final int activity_iask_ianswer=0x7f030000;
     }
     public static final class menu {
-        public static final int iask_ianswer=0x7f060000;
+        public static final int iask_ianswer=0x7f070000;
+    }
+    public static final class raw {
+        public static final int bdspeech_recognition_cancel=0x7f040000;
+        public static final int bdspeech_recognition_error=0x7f040001;
+        public static final int bdspeech_recognition_start=0x7f040002;
+        public static final int bdspeech_recognition_success=0x7f040003;
+        public static final int bdspeech_speech_end=0x7f040004;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
 }
--- src/com/xun/iaskianswer/IAskIAnswerActivity.java
+++ src/com/xun/iaskianswer/IAskIAnswerActivity.java
@@ -1,14 +1,126 @@
 package com.xun.iaskianswer;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.baidu.voicerecognition.android.VoiceRecognitionClient.VoiceClientStatusChangeListener;
+import com.baidu.voicerecognition.android.ui.BaiduASRDigitalDialog;
+import com.baidu.voicerecognition.android.ui.DialogRecognitionListener;
 
 public class IAskIAnswerActivity extends Activity {
+	private TextView tv;
+	private Button btn;
+
+	private DialogRecognitionListener mRecognitionListener;
+	private BaiduASRDigitalDialog mDialog = null;
+	private int mCurrentTheme = Config.DIALOG_THEME;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_iask_ianswer);
+
+		initView();
+		initSpeechListener();
+	}
+
+	private void initSpeechListener() {
+		mRecognitionListener = new DialogRecognitionListener() {
+
+			@Override
+			public void onResults(Bundle results) {
+				ArrayList<String> rs = results != null ? results
+						.getStringArrayList(RESULTS_RECOGNITION) : null;
+				if (rs != null && rs.size() > 0) {
+					tv.setText(rs.get(0));
+				}
+
+			}
+		};
+		btn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				tv.setText(null);
+				// if (mDialog == null || mCurrentTheme != Config.DIALOG_THEME)
+				// {
+				mCurrentTheme = Config.DIALOG_THEME;
+				if (mDialog != null) {
+					mDialog.dismiss();
+				}
+				Bundle params = new Bundle();
+				params.putString(BaiduASRDigitalDialog.PARAM_API_KEY,
+						Constants.API_KEY);
+				params.putString(BaiduASRDigitalDialog.PARAM_SECRET_KEY,
+						Constants.SECRET_KEY);
+				params.putInt(BaiduASRDigitalDialog.PARAM_DIALOG_THEME,
+						Config.DIALOG_THEME);
+				mDialog = new BaiduASRDigitalDialog(IAskIAnswerActivity.this,
+						params);
+				mDialog.setDialogRecognitionListener(mRecognitionListener);
+				// }
+				mDialog.getParams().putInt(BaiduASRDigitalDialog.PARAM_PROP,
+						Config.CURRENT_PROP);
+				mDialog.getParams().putString(
+						BaiduASRDigitalDialog.PARAM_LANGUAGE,
+						Config.getCurrentLanguage());
+				Log.e("DEBUG", "Config.PLAY_START_SOUND = "
+						+ Config.PLAY_START_SOUND);
+				mDialog.getParams().putBoolean(
+						BaiduASRDigitalDialog.PARAM_START_TONE_ENABLE,
+						Config.PLAY_START_SOUND);
+				mDialog.getParams().putBoolean(
+						BaiduASRDigitalDialog.PARAM_END_TONE_ENABLE,
+						Config.PLAY_END_SOUND);
+				mDialog.getParams().putBoolean(
+						BaiduASRDigitalDialog.PARAM_TIPS_TONE_ENABLE,
+						Config.DIALOG_TIPS_SOUND);
+				mDialog.show();
+			}
+		});
+
+	}
+
+	private void initView() {
+		tv = (TextView) findViewById(R.id.tv);
+		btn = (Button) findViewById(R.id.btn);
+	}
+
+	@Override
+	protected void onDestroy() {
+		if (mDialog != null) {
+			mDialog.dismiss();
+		}
+		super.onDestroy();
+	}
+
+	class MyVoiceRecogListener implements VoiceClientStatusChangeListener {
+
+		@Override
+		public void onClientStatusChange(int arg0, Object arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onError(int arg0, int arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onNetworkStatusChange(int arg0, Object arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
 	}
 
 }
